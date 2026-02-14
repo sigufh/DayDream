e803b8475ace0e5e82a5a04e15bab82e81d379aa
sigufh <wy20050306xt@163.com>
2026-02-14 22:03:05 +0800

    Initial Commit

---
+++ daydream/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  daydream
+//
+//  Created by Sigufh on 2026/2/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ daydream/daydreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  daydreamApp.swift
+//  daydream
+//
+//  Created by Sigufh on 2026/2/14.
+//
+
+import SwiftUI
+
+@main
+struct daydreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
